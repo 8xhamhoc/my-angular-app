8f1290cbd9d71f2d43e5ee3206684d972ba45c0a
Quang Phan <tomatoquang@gmail.com>
2026-04-14 06:53:41 +0700

    clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from "./features/dashboard.component";
 import { CounterComponent } from './features/counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, CounterComponent],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
